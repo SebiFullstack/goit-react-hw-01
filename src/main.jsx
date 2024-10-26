3f759cb236488a4ae15a4d7512569341c7699c45
Sebastian4chik <sebi.ashtefcules@gmail.com>
2024-10-26 11:15:50 +0100

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "the-new-css-reset/css/reset.css";
-
-// Імпорт стилів нормалізації
-
-import "modern-normalize";
-import App from './components/App'
 import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
