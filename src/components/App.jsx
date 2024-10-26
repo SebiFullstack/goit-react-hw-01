f20433026099e059f6695452c20ecb02e5b26060
Sebastian4chik <sebi.ashtefcules@gmail.com>
2024-10-26 11:06:39 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import Profile from './Profile/Profile'
 import userData from "../userData.json";
 import FrendList from './FriendList/FriendList';
 import friends from "../friends.json";
-import transactions from "../transactions.json";
 import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from "../transactions.json";
 
 function App() {
   
